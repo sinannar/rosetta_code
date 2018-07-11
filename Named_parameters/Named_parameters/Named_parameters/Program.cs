09ab1a4dc4cf3e37956ebb0f6003f322ba470302
sinan nar <sinan.nar@gmail.com>
2018-07-11 21:54:44 +1200

    Named_parameters

---
+++ Named_parameters/Named_parameters/Named_parameters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Named_parameters
+{
+    class Program
+    {
+        static void AddWidget(string parent, float x = 0, float y = 0, string text = "Default")
+        {
+            Console.WriteLine("parent = {0}, x = {1}, y = {2}, text = {3}", parent, x, y, text);
+        }
+
+        static void Main(string[] args)
+        {
+            AddWidget("root", 320, 240, "First");
+            AddWidget("root", text: "Origin");
+            AddWidget("root", 500);
+            AddWidget("root", text: "Footer", y: 400);
+
+            Console.ReadKey();
+        }
+    }
+}
